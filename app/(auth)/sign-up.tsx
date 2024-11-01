45906fbb1830052c5295fb54f98c85f40a316d0d
hamza bouchtarate <hamza.bouchtarate60@gmail.com>
2024-11-01 08:49:44 +0100

    create signUp file

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 
 const SignUp = () => {
   return (
-    <View>
-      <Text>signUp</Text>
-    </View>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="w-full h-[250px] z-0" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-2 left-5">
+            Create your account
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
